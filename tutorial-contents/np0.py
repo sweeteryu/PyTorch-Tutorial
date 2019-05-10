76321630c9572d9879689a7a43320204e34c097f
Yu <919890413@qq.com>
2019-05-10 22:34:10 +0800

    np1

--- tutorial-contents/np0.py
+++ tutorial-contents/np0.py
@@ -2,14 +2,12 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
 
-a = []
-b = []
-for i in range(6):
-    a.append(i)
-for i in range(6):
-    b.append(a)
-np.save('test2.npy', b)
+#a = []b = []for i in range(6):    a.append(i)for i in range(6):    b.append(a)np.save('test2.npy', b)
 
-test = np.load('adjacency_matrix.npy')
-print(test.ndim)
-print(test.shape)
+#test = np.load('adjacency_matrix.npy')
+#print(test.ndim) print(test.shape)
+
+with open('weibo_network.txt') as file_object:
+    lines = file_object.readlines()
+for line in lines[:5]:
+    print(line)
