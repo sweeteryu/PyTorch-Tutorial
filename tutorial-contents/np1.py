07fe18a59f8d27cf392620d3f96e6472b81aeecd
Yu <919890413@qq.com>
2019-05-12 13:57:06 +0800

    np1read

--- tutorial-contents/np1.py
+++ tutorial-contents/np1.py
@@ -2,16 +2,15 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
 
-a = np.loadtxt('weibo_network.txt')#将test文件作为npy文件读取出来
-print(a)
+#a = np.loadtxt('weibo_network.txt')#将test文件作为npy文件读取出来
+#print(a)
 #np.save('test.npy',a)#存储a为npy文件
 #test=np.load('test.npy')#读取该npy文件
 #print(test)
 
-#with open('odom.txt', 'r') as f:
-#    data = f.readlines()  # txt中所有字符串读入data
+with open('weibo_network.txt', 'r') as f:
+    data = f.readlines()  # txt中所有字符串读入data
 
- #   for line in data:
-  #      odom = line.split()  # 将单个数据分隔开存好
-   #     numbers_float = map(float, odom)  # 转化为浮点数
-    #    print(numbers_float)
+    for line in data:
+        #test = line.split()  # 将单个数据分隔开存好
+        print(line)
